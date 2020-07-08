9ffb6de72d6c156d48b05f229fe6fb65c6f98282
ac360 <austencollins@gmail.com>
2020-07-07 17:26:06 -0700

    move metrics logic to aws-sdk

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@
 
 const path = require('path');
 // eslint-disable-next-line import/no-extraneous-dependencies
-const AWS = require('aws-sdk');
+const AWS = require(`@serverless/aws-sdk`);
 const moment = require('moment');
 const https = require('https');
 
@@ -1224,45 +1224,15 @@ const removeDomain = async (instance, clients) => {
 const getMetrics = async (
   credentials,
   region,
-  roleArn,
+  metaRoleArn,
   apiId,
   functionName,
   rangeStart,
   rangeEnd
 ) => {
-  /**
-   * Validate inputs
-   */
-
-  // Validate: ISO8601 timestamps
-  if (!moment(rangeStart, moment.ISO_8601).isValid()) {
-    throw new Error(`Input "rangeStart" is not a valid IS)8601 timestamp: ${rangeStart}`);
-  }
-  if (!moment(rangeEnd, moment.ISO_8601).isValid()) {
-    throw new Error(`Input "rangeEnd" is not a valid IS)8601 timestamp: ${rangeEnd}`);
-  }
-
-  // Convert to Moment.js objects
-  rangeStart = moment(rangeStart);
-  rangeEnd = moment(rangeEnd);
-
-  // Validate: Start is before End
-  if (rangeStart.isAfter(rangeEnd)) {
-    throw new Error('The "rangeStart" provided is after the "rangeEnd"');
-  }
-
-  // Validate: End is not longer than 30 days
-  if (rangeStart.diff(rangeEnd, 'days') > 32) {
-    throw new Error(
-      `The range cannot be longer than 32 days.  The supplied range is: ${rangeStart.diff(
-        rangeEnd,
-        'days'
-      )}`
-    );
-  }
 
   /**
-   * Create AWS STS Token
+   * Create AWS STS Token via the meta role that is deployed with the Express Component
    */
 
   const sts = new AWS.STS({ region });
@@ -1270,7 +1240,7 @@ const getMetrics = async (
   // Assume Role
   const assumeParams = {};
   assumeParams.RoleSessionName = `session${Date.now()}`;
-  assumeParams.RoleArn = roleArn;
+  assumeParams.RoleArn = metaRoleArn;
   assumeParams.DurationSeconds = 900;
 
   const resAssume = await sts.assumeRole(assumeParams).promise();
@@ -1279,453 +1249,25 @@ const getMetrics = async (
   roleCreds.accessKeyId = resAssume.Credentials.AccessKeyId;
   roleCreds.secretAccessKey = resAssume.Credentials.SecretAccessKey;
   roleCreds.sessionToken = resAssume.Credentials.SessionToken;
-  const cloudwatch = new AWS.CloudWatch({ credentials: roleCreds, region });
 
-  /**
-   * Prepare input range data
-   * - Determine time unit (aka "period"), 1 minute, 1 hour, etc.
-   * - Pre-fill the response values w/ timestamps, since CloudWatch does not return empty bucket values.
-   */
-
-  let period;
-  let timeBuckets;
-  const xData = [];
-  const yData = [];
-  const diffMinutes = Math.ceil(rangeEnd.diff(rangeStart, 'minutes', true)); // 'true' returns decimals
-
-  // Length: 0 mins - 2 hours
-  if (diffMinutes <= 120) {
-    period = 60;
-    rangeStart = rangeStart.startOf('minute');
-    rangeEnd = rangeEnd.endOf('minute');
-    timeBuckets = rangeEnd.diff(rangeStart, 'minutes');
-    // Create values
-    for (let i = 0; i <= timeBuckets; i++) {
-      xData.push(rangeStart.clone().add(i, 'minutes').toISOString());
-      yData.push(0);
+  const resources = [
+    {
+      type: 'aws_http_api',
+      apiId,
+    },
+    {
+      type: 'aws_lambda',
+      functionName,
     }
-  }
-  // Length: 2 hours - 48 hours
-  else if (diffMinutes > 120 && diffMinutes <= 2880) {
-    period = 3600;
-    rangeStart = rangeStart.startOf('hour');
-    rangeEnd = rangeEnd.endOf('hour');
-    timeBuckets = rangeEnd.diff(rangeStart, 'hours');
-    // Create values
-    for (let i = 0; i <= timeBuckets; i++) {
-      xData.push(rangeStart.clone().add(i, 'hours').toISOString());
-      yData.push(0);
-    }
-  }
-  // Length: 48 hours to 32 days
-  else if (diffMinutes > 2880) {
-    period = 86400;
-    rangeStart = rangeStart.startOf('day');
-    rangeEnd = rangeEnd.endOf('day');
-    timeBuckets = rangeEnd.diff(rangeStart, 'days');
-    // Create values
-    for (let i = 0; i <= timeBuckets; i++) {
-      xData.push(rangeStart.clone().add(i, 'days').toISOString());
-      yData.push(0);
-    }
-  }
+  ]
 
-  // Prepare CloudWatch queries
-  const params = {
-    StartTime: rangeStart.unix(),
-    EndTime: rangeEnd.unix(),
-    // NextToken: null, // No need for this since we are restricting value counts.
-    ScanBy: 'TimestampAscending',
-    MetricDataQueries: [
-      {
-        Id: 'api_requests',
-        ReturnData: true,
-        MetricStat: {
-          Metric: {
-            MetricName: 'Count',
-            Namespace: 'AWS/ApiGateway',
-            Dimensions: [
-              {
-                Name: 'Stage',
-                Value: '$default',
-              },
-              {
-                Name: 'ApiId',
-                Value: apiId,
-              },
-            ],
-          },
-          Period: period,
-          Stat: 'Sum',
-        },
-      },
-      {
-        Id: 'api_errors_500',
-        ReturnData: true,
-        MetricStat: {
-          Metric: {
-            MetricName: '5xx',
-            Namespace: 'AWS/ApiGateway',
-            Dimensions: [
-              {
-                Name: 'Stage',
-                Value: '$default',
-              },
-              {
-                Name: 'ApiId',
-                Value: apiId,
-              },
-            ],
-          },
-          Period: period,
-          Stat: 'Sum',
-        },
-      },
-      {
-        Id: 'api_errors_400',
-        ReturnData: true,
-        MetricStat: {
-          Metric: {
-            MetricName: '4xx',
-            Namespace: 'AWS/ApiGateway',
-            Dimensions: [
-              {
-                Name: 'Stage',
-                Value: '$default',
-              },
-              {
-                Name: 'ApiId',
-                Value: apiId,
-              },
-            ],
-          },
-          Period: period,
-          Stat: 'Sum',
-        },
-      },
-      {
-        Id: 'api_latency',
-        ReturnData: true,
-        MetricStat: {
-          Metric: {
-            MetricName: 'Latency',
-            Namespace: 'AWS/ApiGateway',
-            Dimensions: [
-              {
-                Name: 'Stage',
-                Value: '$default',
-              },
-              {
-                Name: 'ApiId',
-                Value: apiId,
-              },
-            ],
-          },
-          Period: period,
-          Stat: 'Average',
-        },
-      },
-      {
-        Id: 'api_data_processed',
-        ReturnData: true,
-        MetricStat: {
-          Metric: {
-            MetricName: 'DataProcessed',
-            Namespace: 'AWS/ApiGateway',
-            Dimensions: [
-              {
-                Name: 'Stage',
-                Value: '$default',
-              },
-              {
-                Name: 'ApiId',
-                Value: apiId,
-              },
-            ],
-          },
-          Period: period,
-          Stat: 'Sum',
-        },
-      },
-      {
-        Id: 'api_integration_latency',
-        ReturnData: true,
-        MetricStat: {
-          Metric: {
-            MetricName: 'IntegrationLatency',
-            Namespace: 'AWS/ApiGateway',
-            Dimensions: [
-              {
-                Name: 'Stage',
-                Value: '$default',
-              },
-              {
-                Name: 'ApiId',
-                Value: apiId,
-              },
-            ],
-          },
-          Period: period,
-          Stat: 'Average',
-        },
-      },
-      {
-        Id: 'function_invocations',
-        ReturnData: true,
-        MetricStat: {
-          Metric: {
-            MetricName: 'Invocations',
-            Namespace: 'AWS/Lambda',
-            Dimensions: [
-              {
-                Name: 'FunctionName',
-                Value: functionName,
-              },
-            ],
-          },
-          Period: period,
-          Stat: 'Sum',
-        },
-      },
-      {
-        Id: 'function_errors',
-        ReturnData: true,
-        MetricStat: {
-          Metric: {
-            MetricName: 'Errors',
-            Namespace: 'AWS/Lambda',
-            Dimensions: [
-              {
-                Name: 'FunctionName',
-                Value: functionName,
-              },
-            ],
-          },
-          Period: period,
-          Stat: 'Sum',
-        },
-      },
-      {
-        Id: 'function_throttles',
-        ReturnData: true,
-        MetricStat: {
-          Metric: {
-            MetricName: 'Throttles',
-            Namespace: 'AWS/Lambda',
-            Dimensions: [
-              {
-                Name: 'FunctionName',
-                Value: functionName,
-              },
-            ],
-          },
-          Period: period,
-          Stat: 'Sum',
-        },
-      },
-      {
-        Id: 'function_duration',
-        ReturnData: true,
-        MetricStat: {
-          Metric: {
-            MetricName: 'Duration',
-            Namespace: 'AWS/Lambda',
-            Dimensions: [
-              {
-                Name: 'FunctionName',
-                Value: functionName,
-              },
-            ],
-          },
-          Period: period,
-          Stat: 'Average',
-        },
-      },
-    ],
-  };
-
-  const data = await cloudwatch.getMetricData(params).promise();
-  console.log(data.MetricDataResults);
-  /**
-   * Prepare response data
-   */
-
-  const result = {
-    rangeStart: rangeStart.toISOString(),
-    rangeEnd: rangeEnd.toISOString(),
-    metrics: [],
-  };
-
-  // Format Results into standard format
-  if (data && data.MetricDataResults) {
-    data.MetricDataResults.forEach((cwMetric) => {
-      // Create metric
-      const metric = {};
-      metric.type = 'bar-v1';
-      metric.stat = null;
-      metric.statText = null;
-      metric.statColor = '#000000';
-      metric.xData = xData.slice();
-      metric.yDataSets = [{}];
-      metric.yDataSets[0].yData = yData.slice();
-
-      // Add Values
-      cwMetric.Timestamps.forEach((cwVal, i) => {
-        metric.xData.forEach((xVal, i2) => {
-          if (moment(xVal).isSame(cwVal)) {
-            metric.yDataSets[0].yData[i2] = cwMetric.Values[i];
-          }
-        });
-      });
-
-      /**
-       * Customize the metric depending on its type
-       */
-
-      // Total Requests
-      if (cwMetric.Id === 'api_requests') {
-        metric.title = 'API Requests';
-        metric.description =
-          'The total number API requests in a given period to your AWS HTTP API.';
-        metric.yDataSets[0].color = '#000000';
-        // Get Sum
-        metric.stat = metric.yDataSets[0].yData.reduce((previous, current) => current + previous);
-        // Add metric
-        result.metrics.push(metric);
-      }
-      // Errors - 5xx
-      if (cwMetric.Id === 'api_errors_500') {
-        metric.title = 'API Errors - 5xx';
-        metric.description =
-          'The number of serverless-side internal errors captured in a given period from your AWS HTTP API most likely generated as a result of issues within your code.';
-        metric.statColor = '#FE5850';
-        metric.yDataSets[0].color = '#FE5850';
-
-        // Get Sum
-        metric.stat = metric.yDataSets[0].yData.reduce((previous, current) => current + previous);
-        // Add metric
-        result.metrics.push(metric);
-      }
-      // Errors - 4xx
-      if (cwMetric.Id === 'api_errors_400') {
-        metric.title = 'API Errors - 4xx';
-        metric.description =
-          'The number of serverless-side client-generated errors captured in a given period from your AWS HTTP API.';
-        metric.statColor = '#FE5850';
-        metric.yDataSets[0].color = '#FE5850';
-        // Get Sum
-        metric.stat = metric.yDataSets[0].yData.reduce((previous, current) => current + previous);
-        // Add metric
-        result.metrics.push(metric);
-      }
-      // Latency
-      if (cwMetric.Id === 'api_latency') {
-        metric.title = 'API Latency';
-        metric.description =
-          'The time between when AWS HTTP API receives a request from a client and when it returns a response to the client. The latency includes the integration latency and other AWS HTTP API overhead.';
-        metric.statColor = '#029CE3';
-        metric.yDataSets[0].color = '#029CE3';
-        // Round Decimals
-        metric.yDataSets[0].yData = metric.yDataSets[0].yData.map((val) => Math.ceil(val));
-        // Get Sum
-        metric.stat = metric.yDataSets[0].yData.reduce((previous, current) => current + previous);
-        // Get Average
-        const filtered = metric.yDataSets[0].yData.filter((x) => x > 0);
-        metric.stat = Math.ceil(metric.stat / filtered.length);
-        metric.statText = 'ms';
-        // Add metric
-        result.metrics.push(metric);
-      }
-      // Integration Latency
-      if (cwMetric.Id === 'api_integration_latency') {
-        metric.title = 'API Integration Latency';
-        metric.description =
-          'The time between when AWS HTTP API relays a request to the backend and when it receives a response from the backend.';
-        metric.statColor = '#029CE3';
-        metric.yDataSets[0].color = '#029CE3';
-        // Round Decimals
-        metric.yDataSets[0].yData = metric.yDataSets[0].yData.map((val) => Math.ceil(val));
-        // Get Sum
-        metric.stat = metric.yDataSets[0].yData.reduce((previous, current) => current + previous);
-        // Get Average
-        const filtered = metric.yDataSets[0].yData.filter((x) => x > 0);
-        metric.stat = Math.ceil(metric.stat / filtered.length);
-        metric.statText = 'ms';
-        // Add metric
-        result.metrics.push(metric);
-      }
-      // Data Processed in Kilobytes
-      if (cwMetric.Id === 'api_data_processed') {
-        metric.title = 'API Data Processed';
-        metric.description = 'The amount of data processed in kilobytes.';
-        metric.statColor = '#000000';
-        metric.yDataSets[0].color = '#000000';
-        // Convert to kilobytes
-        metric.yDataSets[0].yData = metric.yDataSets[0].yData.map((val) => {
-          const kb = val / Math.pow(1024, 1);
-          return Math.round(kb * 100) / 100;
-        });
-        // Get Sum of bytes
-        metric.statText = 'kb';
-        metric.stat = metric.yDataSets[0].yData.reduce((previous, current) => current + previous);
-        // Add metric
-        result.metrics.push(metric);
-      }
-      // Function Invocations
-      if (cwMetric.Id === 'function_invocations') {
-        metric.title = 'Function Invocations';
-        metric.description =
-          'The number of times your function code is executed, including successful executions and executions that result in a function error. Invocations are not recorded if the invocation request is throttled or otherwise resulted in an invocation error. This equals the number of requests billed.';
-        metric.yDataSets[0].color = '#000000';
-        // Get Sum
-        metric.stat = metric.yDataSets[0].yData.reduce((previous, current) => current + previous);
-        // Add metric
-        result.metrics.push(metric);
-      }
-      // Function Errors
-      if (cwMetric.Id === 'function_errors') {
-        metric.title = 'Function Errors';
-        metric.description =
-          'The number of invocations that result in a function error. Function errors include exceptions thrown by your code and exceptions thrown by the Lambda runtime. The runtime returns errors for issues such as timeouts and configuration errors. To calculate the error rate, divide the value of Errors by the value of Invocations.';
-        metric.yDataSets[0].color = '#FE5850';
-        // Get Sum
-        metric.stat = metric.yDataSets[0].yData.reduce((previous, current) => current + previous);
-        // Add metric
-        result.metrics.push(metric);
-      }
-      // Function Throttles
-      if (cwMetric.Id === 'function_throttles') {
-        metric.title = 'Function Throttles';
-        metric.description =
-          'The number of invocation requests that are throttled. When all function instances are processing requests and no concurrency is available to scale up, Lambda rejects additional requests with TooManyRequestsException. Throttled requests and other invocation errors do not count as Invocations or Errors.';
-        metric.yDataSets[0].color = '#FE5850';
-        // Get Sum
-        metric.stat = metric.yDataSets[0].yData.reduce((previous, current) => current + previous);
-        // Add metric
-        result.metrics.push(metric);
-      }
-      // Function Latency
-      if (cwMetric.Id === 'function_duration') {
-        metric.title = 'Function Latency';
-        metric.description =
-          'The amount of time that your function code spends processing an event. For the first event processed by an instance of your function, this includes initialization time. The billed duration for an invocation is the value of Duration rounded up to the nearest 100 milliseconds.';
-        metric.yDataSets[0].color = '#029CE3';
-        metric.statColor = '#029CE3';
-        metric.statText = 'ms';
-        // Round Decimals
-        metric.yDataSets[0].yData = metric.yDataSets[0].yData.map((val) => Math.ceil(val));
-        // Get Sum
-        metric.stat = metric.yDataSets[0].yData.reduce((previous, current) => current + previous);
-        // Get Average
-        const filtered = metric.yDataSets[0].yData.filter((x) => x > 0);
-        metric.stat = Math.ceil(metric.stat / filtered.length);
-        // Add metric
-        result.metrics.push(metric);
-      }
-    });
-  }
-
-  return result;
+  return await AWS.utils.getMetrics({
+    credentials: roleCreds,
+    region,
+    rangeStart,
+    rangeEnd,
+    resources,
+  });
 };
 
 /*
