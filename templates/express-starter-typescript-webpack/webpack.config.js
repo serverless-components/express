436df6f80c3324f38e9b3b231de25e12cda61088
Zack Sheppard <zack@zacksheppard.com>
2020-11-18 14:00:14 +0000

    Fix lint and prettier checks

--- templates/express-starter-typescript-webpack/webpack.config.js
+++ templates/express-starter-typescript-webpack/webpack.config.js
@@ -1,24 +1,26 @@
-const path = require("path");
+'use strict';
+
+const path = require('path');
 
 module.exports = {
-  mode: "production",
-  entry: "./src/app.ts",
+  mode: 'production',
+  entry: './src/app.ts',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
-      }
+      },
     ],
   },
-  target: "node",
+  target: 'node',
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    filename: "app.js",
-    libraryTarget: "commonjs-module",
-    path: path.resolve(__dirname, "build")
-  }
+    filename: 'app.js',
+    libraryTarget: 'commonjs-module',
+    path: path.resolve(__dirname, 'build'),
+  },
 };
