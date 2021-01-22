f10fbf28031299464d900dabdd10f1b746f0fd8c
Eslam A. Hefnawy <eahefnawy@gmail.com>
2021-01-22 21:28:08 +0300

    updated serverless-http package

--- src/_express/handler.js
+++ src/_express/handler.js
@@ -11,48 +11,45 @@ let app = (_req, res) => {
   res.end(`Request received: ${_req.method} - ${_req.path}`);
 };
 
-// NOTICE: require() is relative to this file
 try {
   // eslint-disable-next-line import/no-unresolved
   app = require('..');
 
+  // check if the user forgot to export their express app
   if (typeof app === 'object' && Object.keys(app).length === 0) {
     app = (_req, res) => {
+      const e = new Error(
+        'App not found. Please make sure the app is probably exported from the JS file.'
+      );
+      console.error(e);
       res.statusCode = 404;
-      res.end('App not found. Please make sure the app is probably exported from the JS file.');
-    };
-  }
-} catch (e) {
-  if (e.message.includes('Cannot find module')) {
-    // user probably did not run "npm i". return a helpful message.
-    app = (_req, res) => {
-      res.statusCode = 500;
-      res.end('Did you install all your dependencies within your source folder via npm?');
-    };
-  } else {
-    // some other error such as package.json main not matching filename
-    app = (_req, res) => {
-      res.statusCode = 500;
       res.end(e.stack);
     };
   }
+} catch (e) {
+  // return require error and log it for dev mode
+  app = (_req, res) => {
+    console.error(e);
+    res.statusCode = 500;
+    res.end(e.stack);
+  };
 }
 
-// app.use((err, req, res, next) => {
-//   console.error(err.stack);
-//   res.status(500).send('caught');
-// });
+// default error handler logs errorss for dev mode
+if (typeof app.use === 'function') {
+  // eslint-disable-next-line
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
+    throw err;
+  });
+}
 
 const handle = serverlessHttp(app);
 
 exports.handler = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false;
-  const res = await handle(event, context);
 
-  // log the error for dev mode
-  if (res.statusCode > 300) {
-    console.error(res.body);
-  }
+  const res = await handle(event, context);
 
   return res;
 };
